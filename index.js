0ef6f643705a2c78c615413a3d161a2e1b8948bc
Meroni <114459394+Meroni-bcg@users.noreply.github.com>
2023-02-15 12:56:21 -0800

    form data is getting through

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
+
 // Modules and Globals
-require('dotenv').config()
-const express = require('express')
-const app = express()
+const express = require('express');
+const dotenv = require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT;
 
 // Express Settings
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
 
 // Controllers & Routes
-app.use('/places', require('./controllers/places'))
+app.use('/places', require('./controllers/places'));
 
 app.get('/', (req, res) => {
-    res.render('home')
-})
+	res.render('home');
+});
 
 app.get('*', (req, res) => {
-    res.render('error404')
-})
+	res.render('error404');
+});
 
-// Listen for Connections
-app.listen(process.env.PORT)
+// LISTEN
+app.listen(PORT, () => {
+	console.log('Listening on port:', PORT);
+});
